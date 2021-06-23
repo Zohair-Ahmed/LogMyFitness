45d99c493cc384057a192583c5e5eb4c032bdf5e
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-06-23 18:27:10 -0400

    create exercise done, fix date picker

--- log-my-fitness/src/components/create-exercise.component.js
+++ log-my-fitness/src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css"
 
@@ -23,10 +24,15 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -64,6 +70,10 @@ export default class CreateExercise extends Component {
         }
 
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
+
         window.location = '/';
     }
 
@@ -107,9 +117,11 @@ export default class CreateExercise extends Component {
                     </div>
                     <div className="form-group">
                         <label>Date: </label>
-                        {/* <DatePicker 
-                            selected={this.state.date}
-                            onChange={this.onChangeDuration}/> */}
+                        <div>
+                            {/* <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}/> */}
+                        </div>
                     </div>
                     <div className="form-group">
                         <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
